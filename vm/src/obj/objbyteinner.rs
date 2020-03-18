b07949b6b75ac4d69117604bfcb8cb94ed03de5e
Noah <33094578+coolreader18@users.noreply.github.com>
2020-03-17 20:00:46 -0500

    Fix int.from_bytes() with an arbitrary iterator

--- vm/src/obj/objbyteinner.rs
+++ vm/src/obj/objbyteinner.rs
@@ -40,10 +40,15 @@ impl TryFromObject for PyByteInner {
                 elements: k.try_value().unwrap()
             }),
             l @ PyList => l.get_byte_inner(vm),
-            obj => Err(vm.new_type_error(format!(
-                "a bytes-like object is required, not {}",
-                obj.class()
-            ))),
+            obj => {
+                let iter = vm.get_method_or_type_error(obj.clone(), "__iter__", || {
+                    format!("a bytes-like object is required, not {}", obj.class())
+                })?;
+                let iter = PyIterable::from_method(iter);
+                Ok(PyByteInner {
+                    elements: iter.iter(vm)?.collect::<PyResult<_>>()?,
+                })
+            }
         })
     }
 }
